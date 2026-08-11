a144855ddf00cfb6dd8ce24e381c442b8e741271
waynewonder1 <wayne.wonder@student.moringaschool.com>
2026-08-11 15:37:35 +0300

    done

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [isInStock, setIsInStock] = useState(true);
+
+  function handleClick() {
+    setIsInStock((prev) => !prev);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleClick}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
